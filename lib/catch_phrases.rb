3d97b9b1d9ecb9868f36bb9902a9c214fe8de887
patrick simmons <hadroncollides@gmail.com>
2020-12-29 00:12:17 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,10 +13,6 @@ def link
   puts phrase
 end
 
-describe "all_phrases" do
-  it "puts out all of the previous catch phrases" do
-    expect{all_phrases}.to output(/It's-a me, Mario!\n/).to_stdout
-    expect{all_phrases}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
-    expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
-  end
+def all_phrases (mario, toadstool, link)
+puts all_phrases
 end
