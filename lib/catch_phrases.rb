8c9e441065de7e1e1e7a5e50f65835ed82a86fa9
patrick simmons <hadroncollides@gmail.com>
2020-12-29 00:05:34 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,4 +1,4 @@
-def link
+def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
@@ -6,4 +6,9 @@ end
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+def link
+  phrase = "It's-a me, Mario!"
+  puts phrase
 end
